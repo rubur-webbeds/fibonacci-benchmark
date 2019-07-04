f1d5a34e1c31e3a11d4ee4226de08f33bd6400de
Ruben Bustos <ruben_b_r_@hotmail.com>
2019-07-04 21:56:07 +0200

    Add recursive fibonacci Add recursive fibonacci with memoization

---
+++ FibonacciBenchmark/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FibonacciBenchmark
+{
+    class Program
+    {
+        private static Dictionary<int, int> _cache = new Dictionary<int, int>();
+        private static int _depth;
+
+        static void Main(string[] args)
+        {
+            var num = 7;
+
+            //var result = FibonacciRecursive(num);
+            var result = FibonacciRecursiveCache(num);
+
+            Console.WriteLine("\nPosition: {0} -> Number: {1}", num, result);
+            Console.WriteLine("Depth: {0}", _depth);
+        }
+
+        private static int FibonacciRecursive(int n)
+        {
+            Console.WriteLine("Computing {0}", n);
+            _depth++;
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            return FibonacciRecursive(n - 1) + FibonacciRecursive(n - 2);
+        }
+
+        private static int FibonacciRecursiveCache(int n)
+        {
+            Console.WriteLine("Computing {0}", n);
+            _depth++;
+
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            if (_cache.TryGetValue(n, out int value))
+            {
+                Console.WriteLine("Getting from cache {0}", n);
+
+                return value;
+            }
+
+            value = FibonacciRecursiveCache(n - 1) + FibonacciRecursiveCache(n - 2);
+
+            if(_cache.TryAdd(n, value))
+            {
+                Console.WriteLine("Adding to cache {0}", n);
+            }
+
+            return value;
+        }
+    }
+}
